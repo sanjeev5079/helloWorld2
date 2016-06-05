34e0c39f2e72b1bbddc99ae57403c4d2200f43a2
cloudera <cloudera@quickstart.cloudera>
2016-06-05 04:56:47 -0700

    initial commit

---
+++ helloWorld2/src/helloWorld2/helloWorld.java
@@ -0,0 +1,12 @@
+//Auto build in jenkins - test
+
+package helloWorld2;
+
+public class helloWorld {
+	//comment
+	public static void main(String[] args){
+	System.out.println("Hello World");
+	System.out.println("First change to test Git");
+	
+}
+}
