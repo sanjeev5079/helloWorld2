0b4dbedc593f88ba70b057eecdc815d4523f53f1
cloudera <cloudera@quickstart.cloudera>
2016-06-05 07:04:23 -0700

    test push 2

--- helloWorld2/src/helloWorld2/helloWorld.java
+++ helloWorld2/src/helloWorld2/helloWorld.java
@@ -7,7 +7,7 @@ public class helloWorld {
 	//comment
 	public static void main(String[] args){
 	System.out.println("Hello World");
-	System.out.println("First change to test Git");
+	System.out.prin("First change to test Git");
 	
 }
 }
